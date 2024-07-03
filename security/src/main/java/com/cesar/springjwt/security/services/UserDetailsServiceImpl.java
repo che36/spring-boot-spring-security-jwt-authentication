10b284f0005e389dc917ffc2f7d2cdbc20a12855
Neo40 <tron36.cc@gmail.com>
2024-07-02 21:06:02 -0300

    Adição de pacote services

---
+++ security/src/main/java/com/cesar/springjwt/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cesar.springjwt.security.services;
+
+import com.cesar.springjwt.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
